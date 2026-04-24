beca16057a52b7de7ba124781eba36d7ca5ca2ec
yeony-park <bbling408@naver.com>
2026-04-24 14:31:15 +0900

    featL add credict card PDFs and vision-based text extraction pipeline

---
+++ main.py
@@ -0,0 +1,11 @@
+from src.extraction.extract_01_text_from_pdf import *
+from src.extraction.extract_02_structure_json import *
+from src.extraction.extract_03_chunking import *
+
+if __name__ == "__main__":
+    # extract 순서대로 작성
+
+    # 1. pdf to text
+    # 2. text to structure json
+    # 3. structure json to chunking
+    pass
